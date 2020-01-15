a0902e15b9bd2e73317dc4678b740e7582f3f93e
Petrius Lima <petrius.lima@cassum.dev>
2020-01-15 01:32:08 -0300

    Refactory index.php to increase readability

--- index.php
+++ index.php
@@ -1,27 +1,32 @@
 <?php
+
 //utilização da rule htaccess para gerar uma key com base na url
-$_GET['key'] = (isset($_GET['key']) ? $_GET['key'].'/' : 'index/index');
-$key = $_GET['key'];
+$key = (isset($_GET['key']) ? $_GET['key'].'/' : 'index/index');
+
 //separação das keys a partir do caractere / em arrays
 $separator = explode('/', $key);
+
 //separação delas em variáveis a partir do array
 $controller = $separator[0];
 $action = ($separator[1] == null ? 'index' : $separator[1]);
 
 //carregamento automatico dos models
-function __autoload ($file)
+function __autoload($file)
 {
-    require_once('app/models/'.ucwords($file).'.php');
+    require_once('app/models/' . ucwords($file) . '.php');
 }
 
 //requisição do controller central do framework
 require_once('system/Controller.php');
+
 //requisição do model central do framework
 require_once('system/Model.php');
+
 //requisição dos controllers a partir das keys
-require_once('app/controllers/'.ucwords($controller).'Controller.php');
+require_once('app/controllers/' . ucwords($controller) . 'Controller.php');
 
 //instanciação das classes contidas nos controllers
-$class = (ucwords($controller).'Controller');
-$app = new $class();
+$controllerClassName = ucwords($controller) . 'Controller';
+
+$app = new $controllerClassName();
 $app->$action();
